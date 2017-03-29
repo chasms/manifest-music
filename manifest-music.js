78c5e486aad05040462cf1c74b8f170b6979a9e1
chiasms <chasms@chas.ms>
2017-03-29 15:12:26 -0400

    working with sliders, not finished

--- manifest-music.js
+++ manifest-music.js
@@ -6,25 +6,17 @@ let oscList = []
 let gainNode = audioCtx.createGain()
 
 // create oscillators here:
+
 var oscillator1 = osc()
 var oscillator2 = osc(100, 'triangle')
 
 gainNode.connect(audioCtx.destination) // connect gain node to speakers
 
 // start oscillators here:
+
 oscList.forEach(oscillator => {
   oscillator.start(0)
 })
-
-function osc (freq = 50, type = 'sine', detune = 100) {
-  var oscillator = audioCtx.createOscillator()
-  oscillator.connect(gainNode)
-  oscillator.frequency.value = freq // value in hertz
-  oscillator.type = type // waveform: 'sine', 'square', 'sawtooth', 'triangle' and 'custom'
-  oscillator.detune.value = detune // value in cents
-  oscList.push(oscillator)
-  return oscillator
-}
 
 // mute button
 
@@ -51,6 +43,34 @@ function changeVolume(event) {
   gainNode.gain.value = volumeControl.value
 }
 
+$(".slider").slider().slider("float")
+
 // set gain to volume control slider
 
 gainNode.gain.value = volumeControl.value
+
+$("#flat-slider")
+    .slider({
+        max: 50,
+        min: 0,
+        range: true,
+        values: [15, 35]
+    })
+    .slider("pips", {
+        first: "pip",
+        last: "pip"
+    });
+
+$("#flat-slider-vertical-1, #flat-slider-vertical-2, #flat-slider-vertical-3")
+    .slider({
+        max: 25,
+        min: 0,
+        range: "min",
+        value: 20,
+        orientation: "vertical"
+    })
+    .slider("pips", {
+        first: "pip",
+        last: "pip"
+    })
+    .slider("float");
